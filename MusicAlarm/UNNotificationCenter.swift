4ae8cba87bdc7f530603f5d4a11ebf2c8d55ac3f
iohcrepoleved <aram829@icloud.com>
2023-02-20 16:22:48 +0900

    코드 수정

--- MusicAlarm/UNNotificationCenter.swift
+++ MusicAlarm/UNNotificationCenter.swift
@@ -12,27 +12,30 @@ extension UNUserNotificationCenter {
     func addNotificationRequest(by alarm:Alarm) {
         let content = UNMutableNotificationContent()
         var bookTitle : String = ""
-        let urlStr = "https://openapi.naver.com/v1/search/book.json?query=\(alarm.keyword)&sort=sim"
+        let urlStr = "https://openapi.naver.com/v1/search/book.json?query=\(alarm.keyword)&sort=sim&display=100®"
         
         guard let encodedStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
         let url = URL(string: encodedStr)!
         var urlRequest = URLRequest(url: url)
-        
         urlRequest.allHTTPHeaderFields = [
-            "X-Naver-Client-Id" : "G3hyX6_LFymqGJkp2MV2",
-            "X-Naver-Client-Secret" : "aixVtDNSps"
+            "X-Naver-Client-Id" : "naver api 에서 부여받은 id",
+            "X-Naver-Client-Secret" : "naver api 에서 부여받은 secret key"
         ]
+        
         let session = URLSession(configuration: .default)
         session.dataTask(with: urlRequest) { [weak self] data, response, error in
             guard let data = data, error == nil else {return}
             let decoder = JSONDecoder()
             guard let bookInformation = try? decoder.decode(BookInformation.self, from: data) else {return}
             DispatchQueue.main.async {
-                if let book = bookInformation.items.first {
-                    bookTitle = String(book.title)
+                for i in bookInformation.items {
+                    if i.title.contains(alarm.keyword){
+                        bookTitle = i.title
+                        break
+                    }
                 }
-                content.title = "책 추천 시간 📖"
-                content.body = "\(bookTitle)을 추천해드립니다"
+                content.title = "[\(alarm.keyword)] 책 추천 시간 📖"
+                content.body = "추천 도서명 : \(bookTitle)"
                 content.sound = .default
                 content.badge = 1
                 
@@ -46,5 +49,4 @@ extension UNUserNotificationCenter {
         
         
     }
-
 }
